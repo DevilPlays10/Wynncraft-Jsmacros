f374e8fb1c8b4a88b8da3be5c0124721eac3f1c6
DevilPlays10 <83325644+DevilPlays10@users.noreply.github.com>
2025-08-15 06:01:45 +0500

    initial

---
+++ Main/src/commands/registerer.js
@@ -0,0 +1,29 @@
+const guildCMD = require('./command/guild.js')
+
+const names = JSON.parse((FS.open('../storage/commands/names.json', 'utf-8')).read())
+
+const commands = [
+    Chat.createCommandBuilder('lg').greedyStringArg(names.guildCommand).executes(JavaWrapper.methodToJavaAsync((ent)=>{
+        const name = ent.getArg('name')
+        if (name.match(/[^a-zA-Z ]/g)) {
+            Chat.log(Chat.createTextBuilder()
+                .append(`[GuildLookup]`).withColor(59, 158, 45)
+                .append(` Invalid name`)
+                .append(` "${name}"`).withColor(15, 90, 128)
+            )
+            return true
+        }
+        Chat.log(Chat.createTextBuilder()
+            .append(`[GuildLookup]`).withColor(59, 158, 45)
+            .append(` Searching for guild:`)
+            .append(` "${name}"`).withColor(15, 90, 128)
+        ) 
+        guildCMD(name) //idk how to use await with this and return true within 300ms so this works of rnow
+        return true
+    }))
+]
+
+commands.forEach(ent=>ent.register())
+event.stopListener = JavaWrapper.methodToJava(() => {
+    commands.forEach(ent=>ent.unregister())
+});
