fef54c042b98e97fa9c549815a2b0457a97c55ae
DevilPlays10 <83325644+DevilPlays10@users.noreply.github.com>
2025-08-24 10:37:22 +0500

    fixed terr name not being resolved on ally/closed

--- WarHelper/src/process/Wars2orsmt.js
+++ WarHelper/src/process/Wars2orsmt.js
@@ -11,17 +11,20 @@ const defs = {
 function warmenu(e, conf) {
     const name = e.screen?.getTitleText()?.getString()
     if (!name.startsWith('Attacking: ')) return
-    // Chat.log(e.screen)
-    // // for (let i = 1; i <= e.inventory.getTotalSlots(); i++) {
-    // //     Chat.log(e.inventory.getSlot(i))
-    // //     Chat.log(`count: ${i}`)
-    // // }
     Time.sleep(250)
     const slot = e.inventory.getSlot(13).getLore()
     if (slot[0].getString().includes('Calculating route')) return
     const defence = (slot[0].getString().split(':')[1]).match(/very low|low|medium|high|very high/gi)[0].toLowerCase()
     const cost = (slot[slot.length-2]).getString().match(/(\d+)$/g)[0]
-    const terr = slot[slot.length-4].getString().match(/(?<=§e✔ §f)(.*)(?=§7 )/g, '')[0]
+    // ter name determination
+    let terr;
+    const terr_ = slot[slot.length-4].getString().match(/(?<=✔ §f)(.*)(?=§7 )/g, '')
+    if (!terr_) {
+        // Chat.log("e")
+        terr=slot[slot.length-4].getString().match(/(?<=§7).*/g, '')[0]
+        // Chat.log(terr)
+    } else terr = terr_[0]
+    if (!terr) return
     //autostart
     if (Object.getOwnPropertyNames(defs).includes(conf.AutoStartWarOnMenuDefence.toLowerCase()) &&defs[conf.AutoStartWarOnMenuDefence.toLowerCase()].includes(defence) &&conf.AutoStartWarOnMenuAcceptablePrice>=cost) {
         if (conf.AutoStartWarOnFFA_ONLY&&!isFFA(terr)) return 
